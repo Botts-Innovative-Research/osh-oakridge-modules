4f1a198c33aff4f3845ebd15e112137bd5457451
Drew Botts <drew.botts@gmail.com>
2023-06-14 10:40:43 -0400

    initial building of driver structure

---
+++ sensors/sensorhub-driver-kromek-d3s/src/main/java/com/botts/impl/sensor/kromekd3s/D3sConfig.java
@@ -0,0 +1,18 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+ Copyright (C) 2023 Botts Innovative Research, Inc. All Rights Reserved.
+ ******************************* END LICENSE BLOCK ***************************/
+package com.botts.impl.sensor.kromekd3s;
+
+import org.sensorhub.api.comm.CommProviderConfig;
+import org.sensorhub.api.config.DisplayInfo;
+import org.sensorhub.api.sensor.SensorConfig;
+
+public class D3sConfig extends SensorConfig{
+
+    @DisplayInfo.Required
+    public String serialNumber;
+
+
+    @DisplayInfo(desc="Communication settings to connect to Kromek D3s data stream")
+    public CommProviderConfig<?> commSettings;
+}
---
+++ sensors/sensorhub-driver-kromek-d3s/src/main/java/com/botts/impl/sensor/kromekd3s/HealthStatusOutput.java
@@ -0,0 +1,6 @@
+package com.botts.impl.sensor.kromekd3s;
+
+public class HealthStatusOutput {
+}
+
+// KromekSerialHealthStatusReport
