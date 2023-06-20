443113f431a97a661368cd140460f1fd986ee480
MichaelElmore1 <135364586+MichaelElmore1@users.noreply.github.com>
2023-06-20 15:59:13 -0500

    Initial driver setup

---
+++ sensors/sensorhub-driver-rs350/src/main/java/com/botts/impl/sensor/rs350/RS350Descriptor.java
@@ -0,0 +1,15 @@
+package com.botts.impl.sensor.rs350;
+
+import org.sensorhub.api.module.IModule;
+import org.sensorhub.api.module.IModuleProvider;
+import org.sensorhub.api.module.ModuleConfig;
+import org.sensorhub.impl.module.JarModuleProvider;
+
+public class D3sDescriptor extends JarModuleProvider implements IModuleProvider {
+
+    @Override
+    public Class<? extends IModule<?>> getModuleClass(){return D3sSensor.class;}
+
+    @Override
+    public Class<? extends ModuleConfig> getModuleConfigClass() {return D3sConfig.class;}
+}
---
+++ sensors/sensorhub-driver-rs350/src/main/java/com/botts/impl/sensor/rs350/RS350Sensor.java
@@ -0,0 +1,41 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ Copyright (C) 2021 Botts Innovative Research, Inc. All Rights Reserved.
+
+ ******************************* END LICENSE BLOCK ***************************/
+package com.botts.impl.sensor.rs350;
+
+import org.sensorhub.api.comm.ICommProvider;
+import org.sensorhub.api.common.SensorHubException;
+import org.sensorhub.impl.sensor.AbstractSensorModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.botts.impl.sensor.kromekd3s.D3sConfig;
+
+
+public class D3sSensor extends AbstractSensorModule<D3sConfig> {
+    private static final Logger logger = LoggerFactory.getLogger(D3sSensor.class);
+
+    ICommProvider<?> commProvider;
+
+    public D3sSensor(){
+
+    }
+
+    @Override
+    protected void doInit() throws SensorHubException
+    {
+        super.doInit();
+
+        // generate identifiers: use serial number from config or first characters of local ID
+        generateUniqueID("urn:kromek:d3s:", config.serialNumber);
+        generateXmlID("KROMEK_D3S_", config.serialNumber);
+
+    }
+
+    @Override
+    public boolean isConnected() {
+        return false;
+    }
+}
