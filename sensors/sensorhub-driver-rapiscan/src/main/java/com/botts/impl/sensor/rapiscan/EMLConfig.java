54e73f140032ba6b9d328146cbbf08a279091f41
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2025-05-07 13:11:51 -0500

    Update EMLConfig.java

--- sensors/sensorhub-driver-rapiscan/src/main/java/com/botts/impl/sensor/rapiscan/EMLConfig.java
+++ sensors/sensorhub-driver-rapiscan/src/main/java/com/botts/impl/sensor/rapiscan/EMLConfig.java
@@ -10,7 +10,7 @@ public class EMLConfig {
     @DisplayInfo(label = "Is Collimated", desc = "Collimation status")
     public boolean isCollimated = false;
 
-    @DisplayInfo(label = "Lane Width (m)", desc = "Width of the lane")
+    @DisplayInfo(label = "Lane Width (m)", desc = "Width of the lane in meters")
     public double laneWidth = 4.82f;
 
 }
