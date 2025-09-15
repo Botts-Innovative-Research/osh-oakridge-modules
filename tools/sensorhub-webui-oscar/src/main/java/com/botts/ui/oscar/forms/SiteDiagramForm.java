4ee248b55124d2a66d1bc4d54f1c77d59eb03169
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-15 11:09:50 -0500

    Create SiteDiagramForm.java

---
+++ tools/sensorhub-webui-oscar/src/main/java/com/botts/ui/oscar/forms/SiteDiagramForm.java
@@ -0,0 +1,61 @@
+package com.botts.ui.oscar.forms;
+
+
+import com.botts.impl.system.database.OccupancyVideoPurgePolicyConfig;
+import com.vaadin.event.MouseEvents;
+import com.vaadin.server.FileResource;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Image;
+import com.vaadin.v7.data.Property;
+import com.vaadin.v7.ui.Field;
+import org.sensorhub.ui.FieldWrapper;
+import org.sensorhub.ui.GenericConfigForm;
+import org.sensorhub.ui.SystemDriverDatabaseConfigForm;
+import org.sensorhub.ui.data.BaseProperty;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * @author
+ * @since
+ */
+public class SiteDiagramForm extends GenericConfigForm {
+
+    @Override
+    protected Field<?> buildAndBindField(String label, String propId, Property<?> prop) {
+        Field<?> field = super.buildAndBindField(label, propId, prop);
+
+        if (propId.endsWith("box_UL") || propId.equals("box_UL")) {
+            System.out.println("box ll");
+           return addImage(field, "");
+        }
+
+        return field;
+    }
+
+    private Field<?> addImage(Field<?> field, String imageUrl){
+
+        return new FieldWrapper<Object>((Field<Object>) field){
+
+            @Override
+            protected Component initContent()
+            {
+                HorizontalLayout layout = new HorizontalLayout();
+                layout.setSpacing(true);
+
+                Image image = new Image();
+                image.setSource(new FileResource(new File("SiteMap.png")));
+                image.addClickListener((MouseEvents.ClickListener) event -> {
+                    var lon = event.getRelativeX();
+                    var lat = event.getRelativeY();
+
+                });
+
+                layout.addComponent(image);
+                return layout;
+            }
+        };
+    }
+}
