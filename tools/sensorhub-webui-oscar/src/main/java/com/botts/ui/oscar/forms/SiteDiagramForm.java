d470d05909a65bc3d9a7d2c650b3be610e77ee34
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-17 08:55:37 -0500

    Update SiteDiagramForm.java

--- tools/sensorhub-webui-oscar/src/main/java/com/botts/ui/oscar/forms/SiteDiagramForm.java
+++ tools/sensorhub-webui-oscar/src/main/java/com/botts/ui/oscar/forms/SiteDiagramForm.java
@@ -8,26 +8,37 @@ import com.vaadin.ui.Image;
 import com.vaadin.ui.Label;
 import com.vaadin.v7.data.Property;
 import com.vaadin.v7.ui.Field;
+import com.vaadin.v7.ui.TextField;
+import org.sensorhub.api.sensor.PositionConfig;
 import org.sensorhub.ui.GenericConfigForm;
-
-import java.awt.*;
 import java.io.File;
+import java.util.List;
 
 /**
  * @author
  * @since
  */
 public class SiteDiagramForm extends GenericConfigForm {
+    private transient PositionConfig config;
+    private TextField latField;
+    private TextField lonField;
+
 
     @Override
     protected Field<?> buildAndBindField(String label, String propId, Property<?> prop) {
         Field<?> field = super.buildAndBindField(label, propId, prop);
 
         if (propId.equals("location.lon")) {
-            var image = addImage("web/sitemaps/image.png");
-            super.addComponents(image);
+            VerticalLayout layout = addImage("web/sitemaps/image.png");
+
+            super.addComponents(layout);
+
+            lonField = (TextField) field;
         }
 
+        if(propId.equals("location.lat")) {
+            latField = (TextField) field;
+        }
         return field;
     }
 
@@ -36,24 +47,49 @@ public class SiteDiagramForm extends GenericConfigForm {
         final Image siteMap = new Image();
         siteMap.setSource(new FileResource(new File("web/sitemaps/image.png")));
 
-        HorizontalLayout coordinateLayout = new HorizontalLayout();
+        siteMap.getWidth();
+        siteMap.getHeight();
 
+        HorizontalLayout coordinateLayout = new HorizontalLayout();
         Label pixelCoordinatesTitle = new Label("Coordinates: ");
         Label pixelCoordinates = new Label("");
         coordinateLayout.addComponents(pixelCoordinatesTitle, pixelCoordinates);
         layout.addComponent(coordinateLayout);
-//        layout.addComponent(new Label("Clicked at x: " + ", y: " ));
 
         siteMap.addClickListener((MouseEvents.ClickListener) event -> {
-            var lon = event.getRelativeX();
-            var lat = event.getRelativeY();
+            int lon = event.getRelativeX();
+            int lat = event.getRelativeY();
             System.out.println(lon + " " + lat);
 
             pixelCoordinates.setValue(lon + ", " + lat);
 
+            lonField.setValue(String.valueOf(lon));
+            latField.setValue(String.valueOf(lat));
+
         });
 
         layout.addComponent(siteMap);
+
         return layout;
+    }
+
+
+    // x = longitude
+    // y = latitude
+    // [x,y] = [lon, lat]
+
+    /**
+     * @param image_width the width of the sitemap uploaded
+     * @param image_height the height of the sitemap uploaded
+     * @param lowerLeftBound the lower left of the bounding box coordinates
+     * @param upperRightBound the upper right of the bounding box coordinates
+     * @param pixel_x the pixel_x given from clicking the sitemap
+     * @param pixel_y the pixel_y given from clicking the sitemap
+     */
+    private List calcLinearMapping(int image_width, int image_height, int[] lowerLeftBound, int[] upperRightBound, int pixel_x, int pixel_y) {
+        double longitude = (pixel_x - lowerLeftBound[0]) / (upperRightBound[1] - lowerLeftBound[0]) * image_width;
+        double latitude = (upperRightBound[1] - pixel_y) / (upperRightBound[1] - lowerLeftBound[1]) * image_height;
+
+        return List.of(longitude,latitude);
     }
 }
