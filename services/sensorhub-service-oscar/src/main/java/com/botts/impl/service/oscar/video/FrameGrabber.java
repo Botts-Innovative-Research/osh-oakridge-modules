c3648c39954069d0da8d88b3c6aa33ac1a8bb12c
kyle-fitzp <kyle.fitzpatrick@botts-inc.com>
2025-09-18 09:53:43 -0500

    Basic video retention structure

---
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/video/FrameGrabber.java
@@ -0,0 +1,11 @@
+package com.botts.impl.service.oscar.video;
+
+public class FrameGrabber {
+    final int frameCount;
+
+    FrameGrabber(int frameCount) {
+        this.frameCount = frameCount;
+    }
+
+
+}
