a78462b73e3a4a9411b6423a4cea62272cad81f7
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-21 10:52:13 -0500

    updated reports to fetch obs from dbs and create tables
    # Conflicts:
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/OSCARServiceModule.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/RequestReportControl.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/TableGenerator.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/Utils.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/AdjudicationReport.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/EventReportTodo.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/LaneReport.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/RDSReport.java

---
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/EventReportTodo.java
@@ -0,0 +1,92 @@
+package com.botts.impl.service.oscar.reports.types;
+
+import com.botts.impl.service.oscar.OSCARServiceModule;
+import com.botts.impl.service.oscar.reports.helpers.ReportType;
+import com.botts.impl.service.oscar.reports.helpers.TableGenerator;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import org.sensorhub.api.database.IDatabaseRegistry;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+
+public class EventReportTodo extends Report {
+
+    String reportTitle = "Event Report";
+
+    Document document;
+    PdfWriter pdfWriter;
+    PdfDocument pdfDocument;
+    String pdfFileName;
+
+    OSCARServiceModule module;
+    TableGenerator tableGenerator;
+
+    public EventReportTodo(Instant startTime, Instant endTime, String eventID, String laneId, OSCARServiceModule module) {
+        try {
+
+            pdfFileName = ReportType.EVENT.name()+ "_" + laneId + "_" + eventID + "_" + startTime + "_"+ endTime + ".pdf";
+            File file = new File( "files/reports/" + pdfFileName);
+            file.getParentFile().mkdirs();
+
+            pdfWriter  = new PdfWriter(file);
+            pdfDocument = new PdfDocument(pdfWriter);
+            document = new Document(pdfDocument);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.module = module;
+        tableGenerator = new TableGenerator(document);
+    }
+
+    @Override
+    public String generate() {
+        addHeader();
+
+        // check for data before calling these
+//        addAlarmStatistics();
+//        addFaultStatistics();
+//        addStatistics();
+//        addOccupancyStatistics();
+
+        document.close();
+        pdfDocument.close();
+
+        return pdfFileName;
+    }
+
+    private void addHeader(){
+        document.add(new Paragraph(reportTitle).setFontSize(16).simulateBold());
+        document.add(new Paragraph("Event Report").setFontSize(16).simulateBold());
+        document.add(new Paragraph("Lane ID:" + "TODO: lane name").setFontSize(12));
+        document.add(new Paragraph("Event ID:" + "TODO: lane name").setFontSize(12));
+        document.add(new Paragraph("\n"));
+    }
+
+    private void addOccupancyStatistics(){
+        document.add(new Paragraph("Occupancy Statistics").setFontSize(12));
+//        addTableToPdf(alarmOccupancyHeaders, null);
+
+    }
+
+    private void addAlarmStatistics(){
+        document.add(new Paragraph("Alarm Statistics").setFontSize(12));
+        //        addTableToPdf(alarmOccupancyHeaders, null);
+
+    }
+
+    private void addFaultStatistics(){
+        document.add(new Paragraph("Fault Statistics").setFontSize(12));
+        //        addTableToPdf(alarmOccupancyHeaders, null);
+
+    }
+
+    private void addStatistics(){
+        document.add(new Paragraph("Statistics").setFontSize(12));
+        //        addTableToPdf(alarmOccupancyHeaders, null);
+
+    }
+}
