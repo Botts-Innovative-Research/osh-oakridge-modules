6cc06c3cbbb0790fb552c8282beb3fada02a6296
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-21 11:30:08 -0500

    start writing tests.. (needs work)

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/AdjudicationReport.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/AdjudicationReport.java
@@ -1,7 +1,8 @@
 package com.botts.impl.service.oscar.reports.types;
 
 import com.botts.impl.service.oscar.OSCARServiceModule;
-import com.botts.impl.service.oscar.reports.helpers.ReportType;
+import com.botts.impl.service.oscar.reports.helpers.ChartGenerator;
+import com.botts.impl.service.oscar.reports.helpers.ReportCmdType;
 import com.botts.impl.service.oscar.reports.helpers.TableGenerator;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
@@ -22,6 +23,7 @@ public class AdjudicationReport extends Report {
 
     OSCARServiceModule module;
     TableGenerator tableGenerator;
+    ChartGenerator chartGenerator;
 
 //    String[] dispositionHeaders =  {
 //            "Real Alarm - Other", "False Alarm - Other", "Physical Inspection Negative", "Innocent Alarm - Medical Isotope Found", "Innocent Alarm - Declared Shipment of Radioactive Material", "No Disposition", "False Alarm - RIID/ASP Indicates Background Only", "Real Alarm - Contraband Found", "Tamper/Fault - Unauthorized Activity", "Alarm/Tamper/Fault- Authorized Test/Maintenance/Training Activity", "Alarm - Naturally Occurring Radioactive Material (NORM) Found"
@@ -33,7 +35,7 @@ public class AdjudicationReport extends Report {
 
     public AdjudicationReport(Instant startTime, Instant endTime, String eventId, String laneId, OSCARServiceModule module) {
         try {
-            pdfFileName = ReportType.ADJUDICATION.name() + "_" + startTime + "_"+ endTime + ".pdf";
+            pdfFileName = ReportCmdType.ADJUDICATION.name() + "_" + startTime + "_"+ endTime + ".pdf";
             File file = new File("files/reports/" + pdfFileName);
             file.getParentFile().mkdirs();
 
