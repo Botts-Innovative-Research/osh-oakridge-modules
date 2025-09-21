3e832ee3f1a31b0ffd997e7ce8639bfdc070f1e5
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-21 10:52:13 -0500

    updated reports to fetch obs from dbs and create tables
    # Conflicts:
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/OSCARServiceModule.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/RequestReportControl.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/TableGenerator.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/Utils.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/AdjudicationReport.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/EventReportTodo.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/LaneReport.java
    #	services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/RDSReport.java

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/LaneReport.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/types/LaneReport.java
@@ -1,58 +1,52 @@
 package com.botts.impl.service.oscar.reports.types;
 
 import com.botts.impl.service.oscar.OSCARServiceModule;
+import com.botts.impl.service.oscar.reports.helpers.ReportType;
 import com.botts.impl.service.oscar.reports.helpers.TableGenerator;
-import com.botts.impl.service.oscar.reports.helpers.Utils;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.Paragraph;
-import org.sensorhub.api.data.IObsData;
-import org.sensorhub.impl.utils.rad.RADHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.sensorhub.api.datastore.obs.DataStreamFilter;
+import org.sensorhub.api.datastore.obs.ObsFilter;
 
 import java.io.File;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Predicate;
 
+import static com.botts.impl.service.oscar.reports.helpers.Constants.*;
+import static com.botts.impl.service.oscar.reports.helpers.Constants.DEF_ALARM;
+import static com.botts.impl.service.oscar.reports.helpers.Constants.DEF_GAMMA;
+import static com.botts.impl.service.oscar.reports.helpers.Constants.DEF_NEUTRON;
 
 public class LaneReport extends Report {
 
-    private static final Logger log = LoggerFactory.getLogger(LaneReport.class);
     String reportTitle = "Lane Report";
-
     Document document;
+    PdfWriter pdfWriter;
     PdfDocument pdfDocument;
     String pdfFileName;
 
-    String laneUID;
-    Instant begin;
-    Instant end;
     OSCARServiceModule module;
     TableGenerator tableGenerator;
 
-    public LaneReport(String filePath, Instant startTime, Instant endTime, String laneUID, OSCARServiceModule module) {
+    public LaneReport(Instant startTime, Instant endTime, String laneId, OSCARServiceModule module) {
         try {
-            pdfFileName = filePath;
-            File file = new File(pdfFileName);
+            pdfFileName = ReportType.LANE.name() + "_" + laneId + "_" + startTime + "_"+ endTime + ".pdf";
+            File file = new File("files/reports/" + pdfFileName);
             file.getParentFile().mkdirs();
 
-            pdfDocument = new PdfDocument(new PdfWriter(file));
+            pdfWriter  = new PdfWriter(file);
+
+            pdfDocument = new PdfDocument(pdfWriter);
             document = new Document(pdfDocument);
         } catch (IOException e) {
-            log.error(e.getMessage(), e);
-            return;
+            throw new RuntimeException(e);
         }
 
-        this.laneUID = laneUID;
         this.module = module;
-        this.begin = startTime;
-        this.end = endTime;
-        this.tableGenerator = new TableGenerator();
+        tableGenerator = new TableGenerator(document);
     }
 
     @Override
@@ -65,56 +59,70 @@ public class LaneReport extends Report {
         document.close();
         pdfDocument.close();
 
-        tableGenerator = null;
         return pdfFileName;
+
     }
 
     private void addHeader(){
         document.add(new Paragraph(reportTitle).setFontSize(16).simulateBold());
+        document.add(new Paragraph("Lane Report").setFontSize(16).simulateBold());
         document.add(new Paragraph("Lane Name: " + "TODO: lane name").setFontSize(12));
         document.add(new Paragraph("\n"));
     }
 
 
     private void addAlarmStatistics(){
-
         document.add(new Paragraph("Alarm Statistics"));
 
-        Map<String, String> alarmOccCounts = new HashMap<>();
+        HashMap<String, Double> alarmOccCounts = new HashMap<>();
 
-        Predicate<IObsData> gammaNeutronPredicate = (obsData) -> obsData.getResult().getBooleanValue(5) && obsData.getResult().getBooleanValue(6);
-        Predicate<IObsData> gammaPredicate = (obsData) -> obsData.getResult().getBooleanValue(5) && !obsData.getResult().getBooleanValue(6);
-        Predicate<IObsData> neutronPredicate = (obsData) -> !obsData.getResult().getBooleanValue(5) && obsData.getResult().getBooleanValue(6);
-        Predicate<IObsData> occupancyTotalPredicate = (obsData) -> true;
-//        Predicate<IObsData> occupancyNonAlarmingPredicate = (obsData) -> !obsData.getResult().getBooleanValue(5) && !obsData.getResult().getBooleanValue(6);
-//        Predicate<IObsData> emlSuppPredicate = (obsData) -> {return obsData.getResult();};
+        double gammaAlarmCount = 0;
+        double neutronAlarmCount = 0;
+        double gammaNeutronAlarmCount = 0;
+        double emlSuppressedCount = 0;
+        double totalOccupancyCount = 0;
 
+        double alarmOccupancyAverage = 0;
+        double emlSuppressedAverage = 0;
 
-        long gammaNeutronAlarmCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module, gammaNeutronPredicate, begin, end);
-        long gammaAlarmCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module ,gammaPredicate, begin, end);
-        long neutronAlarmCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module, neutronPredicate, begin, end);
-        long totalOccupancyCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module, occupancyTotalPredicate, begin, end);
-//        long nonAlarmingOccupancyCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module, occupancyNonAlarmingPredicate, begin, end);
-//        long emlSuppressedCount= Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_OCCUPANCY}, module, emlSuppPredicate);
+        // TODO: also need to sort out from specific LANE
 
-        long totalAlarmingCount = gammaAlarmCount + neutronAlarmCount + gammaNeutronAlarmCount;
+        var query = module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
+                .withDataStreams(new DataStreamFilter.Builder()
+                        .withObservedProperties(DEF_OCCUPANCY)
+                        .build())
+                .build()).iterator();
 
-        long alarmOccupancyAverage = Utils.calculateAlarmingOccRate(totalAlarmingCount, totalOccupancyCount);
-//        long emlSuppressedAverage = Utils.calcEMLAlarmRate(emlSuppressedCount, totalAlarmingCount);
+        while (query.hasNext()) {
+            var entry = query.next();
 
+            var result = entry.getResult();
 
-        alarmOccCounts.put("Gamma Alarm", String.valueOf(gammaAlarmCount));
-        alarmOccCounts.put("Neutron Alarm", String.valueOf(neutronAlarmCount));
-        alarmOccCounts.put("Gamma-Neutron Alarm", String.valueOf(gammaNeutronAlarmCount));
-//        alarmOccCounts.put("Non-Alarming Occupancies", String.valueOf(nonAlarmingOccupancyCount));
-        alarmOccCounts.put("Total Occupancies", String.valueOf(totalOccupancyCount));
-        alarmOccCounts.put("Alarm Occupancy Rate", String.valueOf(alarmOccupancyAverage));
+            var gammaAlarm = result.getBooleanValue(5);
+            var neutronAlarm = result.getBooleanValue(6);
 
-        //        alarmOccCounts.put("EML Suppressed", emlSuppressedCount);
-//        alarmOccCounts.put("EML Alarm Rate", emlSuppressedAverage);
+            if (gammaAlarm && neutronAlarm) {
+                gammaNeutronAlarmCount++;
 
-        var table = tableGenerator.addTable(alarmOccCounts);
-        document.add(table);
+            }  else if (gammaAlarm && !neutronAlarm) {
+                gammaAlarmCount++;
+            }   else if (!gammaAlarm && neutronAlarm) {
+                neutronAlarmCount++;
+            }
+
+        }
+
+//        double alarmRate = Utils.calculateAlarmingOccRate()
+
+        alarmOccCounts.put("Gamma", gammaAlarmCount);
+        alarmOccCounts.put("Neutron", neutronAlarmCount);
+        alarmOccCounts.put("Gamma-Neutron", gammaNeutronAlarmCount);
+        alarmOccCounts.put("EML Suppressed", emlSuppressedCount);
+        alarmOccCounts.put("Alarm Occupancy Rate", alarmOccupancyAverage);
+        alarmOccCounts.put("Total Occupancies", totalOccupancyCount);
+        alarmOccCounts.put("EML Suppressed Rate", emlSuppressedAverage);
+
+        tableGenerator.addTable(alarmOccCounts);
 
         document.add(new Paragraph("\n"));
     }
@@ -122,37 +130,82 @@ public class LaneReport extends Report {
     private void addFaultStatistics(){
         document.add(new Paragraph("Fault Statistics"));
 
-        HashMap<String, String> faultCounts = new HashMap<>();
+        HashMap<String, Double> faultCounts = new HashMap<>();
+
+        double tamperCount = 0;
+        double extendedOccupancyCount = 0;
+        double commsCount = 0;
+        double camCount = 0;
+        double gammaHighFaultCount = 0;
+        double gammaLowFaultCount = 0;
+        double neutronHighFaultCount = 0;
 
 
-        Predicate<IObsData> tamperPredicate = (obsData) -> obsData.getResult().getBooleanValue(1);
-        Predicate<IObsData> gammaHighPredicate = (obsData) -> obsData.getResult().getStringValue(1).equals("Fault - Gamma High");
-        Predicate<IObsData> gammaLowPredicate = (obsData) -> obsData.getResult().getStringValue(1).equals("Fault - Gamma Low");
-        Predicate<IObsData> neutronHighPredicate = (obsData) -> obsData.getResult().getStringValue(1).equals("Fault - Neutron High");
-//        Predicate<IObsData> commsPredicate = (obsData) -> {return obsData.getResult().getBooleanValue(1);};
-//        Predicate<IObsData> cameraPredicate = (obsData) -> {return obsData.getResult().getBooleanValue(1);};
-//        Predicate<IObsData> extendedOccPredicate = (obsData) -> {return obsData.getResult().getBooleanValue(1);};
+        var query = module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
+                .withDataStreams(new DataStreamFilter.Builder()
+
+                        .withObservedProperties(DEF_TAMPER)
+                        .build())
+                .build()).iterator();
+
+        while (query.hasNext()) {
+            var entry = query.next();
+
+            var result = entry.getResult();
+
+            var tamper = result.getBooleanValue(1);
+            if(tamper){
+                tamperCount++;
+            }
+        }
+
+        var query2 = module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
+                .withDataStreams(new DataStreamFilter.Builder()
+                        .withObservedProperties(DEF_ALARM, DEF_GAMMA)
+                        .build())
+                .build()).iterator();
+
+        while (query2.hasNext()) {
+            var entry = query2.next();
+            var result = entry.getResult();
+            var alarmState = result.getStringValue(1);
+
+            if(alarmState == "Fault - Gamma High"){
+                gammaHighFaultCount++;
+            }
+            else if(alarmState == "Fault - Gamma Low"){
+                gammaLowFaultCount++;
+            }
+        }
+
+        var query3 = module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
+                .withDataStreams(new DataStreamFilter.Builder()
+                        
+                        .withObservedProperties(DEF_ALARM, DEF_NEUTRON)
+                        .build())
+                .build()).iterator();
+
+        while (query3.hasNext()) {
+            var entry = query2.next();
+            var result = entry.getResult();
+            var alarmState = result.getStringValue(1);
+
+            if(alarmState == "Fault - Neutron High"){
+                neutronHighFaultCount++;
+            }
+        }
 
 
-        long tamperCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_TAMPER}, module, tamperPredicate, begin, end);
-        long gammaHighFaultCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_GAMMA, RADHelper.DEF_ALARM}, module,gammaHighPredicate, begin, end);
-        long gammaLowFaultCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_GAMMA, RADHelper.DEF_ALARM}, module,gammaLowPredicate, begin, end);
-        long neutronHighFaultCount = Utils.countObservationsFromLane(laneUID, new String[]{RADHelper.DEF_NEUTRON, RADHelper.DEF_ALARM}, module,neutronHighPredicate, begin, end);
-
-//        long extendedOccupancyCount = Utils.countObservationsFromLane(laneUID, RADHelper.DEF_TAMPER, module,extendedOccPredicate, begin, end);
-
-
-        faultCounts.put("Tamper", String.valueOf(tamperCount));
-//        faultCounts.put("Extended Occupancy", extendedOccupancyCount);
-//        faultCounts.put("Comm", commsCount);
-//        faultCounts.put("Camera", camCount);
-        faultCounts.put("Gamma-High", String.valueOf(gammaHighFaultCount));
-        faultCounts.put("Gamma-Low", String.valueOf(gammaLowFaultCount));
-        faultCounts.put("Neutron-High", String.valueOf(neutronHighFaultCount));
+        faultCounts.put("Tamper", tamperCount);
+        faultCounts.put("Extended Occupancy", extendedOccupancyCount);
+        faultCounts.put("Comm", commsCount);
+        faultCounts.put("Camera", camCount);
+        faultCounts.put("Gamma-High", gammaHighFaultCount);
+        faultCounts.put("Gamma-Low", gammaLowFaultCount);
+        faultCounts.put("Neutron-High", neutronHighFaultCount);
 
         tableGenerator.addTable(faultCounts);
 
         document.add(new Paragraph("\n"));
     }
-
 }
