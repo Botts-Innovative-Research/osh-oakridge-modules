e5e9ba089a20ed38b56929f32ddb0911ad4927bf
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-22 15:46:42 -0500

    update report

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/Utils.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/Utils.java
@@ -1,35 +1,51 @@
 package com.botts.impl.service.oscar.reports.helpers;
 
 import com.botts.impl.service.oscar.OSCARServiceModule;
+import org.sensorhub.api.data.IObsData;
 import org.sensorhub.api.datastore.obs.DataStreamFilter;
-import org.sensorhub.api.datastore.obs.IObsStore;
 import org.sensorhub.api.datastore.obs.ObsFilter;
+import org.sensorhub.api.datastore.system.SystemFilter;
 
-import java.util.stream.Stream;
+import java.time.Instant;
+import java.util.function.Predicate;
 
-import static com.botts.impl.service.oscar.reports.helpers.Constants.DEF_ALARM;
-import static com.botts.impl.service.oscar.reports.helpers.Constants.DEF_NEUTRON;
 
 public class Utils {
 
-    public static double calcEMLAlarmRate(double emlSuppCount, double alarmCount){
-        if (emlSuppCount + alarmCount == 0) return 0.0;
+    public static long calcEMLAlarmRate(long emlSuppCount, long alarmCount){
+        if (emlSuppCount + alarmCount == 0) return 0;
 
         return emlSuppCount / (emlSuppCount + alarmCount);
     }
 
-    public static double calculateAlarmingOccRate(double alarmCount, double occupancyCount){
-        if(occupancyCount == 0) return 0.0;
+    public static long calculateAlarmingOccRate(long alarmCount, long occupancyCount){
+        if(occupancyCount == 0) return 0;
 
         return alarmCount / occupancyCount;
     }
 
-    public static Object queryDatabase(String observedProperties, OSCARServiceModule module){
+    public static long countObservations(String[] observedProperties, OSCARServiceModule module, Predicate<IObsData> valuePredicate, Instant begin, Instant end){
         return module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
                 .withDataStreams(new DataStreamFilter.Builder()
                         .withObservedProperties(observedProperties)
+                        .withValidTimeDuring(begin, end)
                         .build())
-                .build());
+                        .withValuePredicate(valuePredicate)
+                .build()).count();
+    }
+
+    public static long countObservationsFromLane(String laneUID, String[] observedProperties, OSCARServiceModule module, Predicate<IObsData> valuePredicate,Instant begin, Instant end){
+        return module.getParentHub().getDatabaseRegistry().getFederatedDatabase().getObservationStore().select(new ObsFilter.Builder()
+                .withSystems(new SystemFilter.Builder()
+                        .withUniqueIDs(laneUID)
+                        .build())
+                .withDataStreams(new DataStreamFilter.Builder()
+                        .withObservedProperties(observedProperties)
+                        .withValidTimeDuring(begin, end)
+                        .build())
+                .withValuePredicate(valuePredicate)
+                .build())
+                .count();
     }
 
 }
