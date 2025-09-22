aba8636f77cb28a8ee1427b761229b8a6aca3dd9
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-22 15:46:42 -0500

    update report

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/TableGenerator.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/TableGenerator.java
@@ -5,6 +5,7 @@ import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.UnitValue;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class TableGenerator {
     Document document;
@@ -13,12 +14,12 @@ public class TableGenerator {
         this.document = document;
     }
 
-    public void addTable(HashMap<String, Double> tableData){
+    public void addTable(Map<String, String> tableData){
         Table table = new Table(UnitValue.createPercentArray(8)).useAllAvailableWidth();
 
         for(String key : tableData.keySet()){
             table.addHeaderCell(key);
-            table.addCell(tableData.get(key).toString());
+            table.addCell(tableData.get(key));
         }
 
         document.add(table);
