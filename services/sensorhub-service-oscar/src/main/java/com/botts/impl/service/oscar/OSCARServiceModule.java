c79ca1ed0ffa4293971317ae12e8f1071a480daf
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-25 11:41:56 -0500

    updates to sitemap

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/OSCARServiceModule.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/OSCARServiceModule.java
@@ -19,6 +19,8 @@ import com.botts.impl.service.oscar.adjudication.AdjudicationControl;
 import com.botts.impl.service.oscar.clientconfig.ClientConfigOutput;
 import com.botts.impl.service.oscar.reports.RequestReportControl;
 import com.botts.impl.service.oscar.siteinfo.SiteInfoOutput;
+import com.botts.impl.service.oscar.siteinfo.SitemapDiagramHandler;
+import com.botts.impl.service.oscar.spreadsheet.SpreadsheetHandler;
 import org.sensorhub.api.common.SensorHubException;
 import org.sensorhub.api.database.IObsSystemDatabase;
 import org.sensorhub.impl.module.AbstractModule;
@@ -29,8 +31,10 @@ public class OSCARServiceModule extends AbstractModule<OSCARServiceConfig> {
     AdjudicationControl adjudicationControl;
 
     ClientConfigOutput clientConfigOutput;
-
+    SpreadsheetHandler spreadsheetHandler;
+    SitemapDiagramHandler sitemapDiagramHandler;
     OSCARSystem system;
+
     @Override
     protected void doInit() throws SensorHubException {
         super.doInit();
@@ -40,24 +44,26 @@ public class OSCARServiceModule extends AbstractModule<OSCARServiceConfig> {
 
         // TODO: Add or update report generation control interface
 
+
+        spreadsheetHandler = new SpreadsheetHandler(getParentHub());
+        if (config.spreadsheetConfigPath != null && !config.spreadsheetConfigPath.isEmpty())
+            spreadsheetHandler.handleFile(config.spreadsheetConfigPath);
+
         // TODO: Add or update site info datastream
+        sitemapDiagramHandler = new SitemapDiagramHandler(getParentHub());
+        if(config.siteDiagramConfig.siteDiagramPath != null && !config.siteDiagramConfig.siteDiagramPath.isEmpty())
+            sitemapDiagramHandler.handleFile(config.siteDiagramConfig.siteDiagramPath);
 
         createOutputs();
         createControls();
 
         system.updateSensorDescription();
-        getParentHub().getSystemDriverRegistry().register(system);
-
-        var module = getParentHub().getModuleRegistry().getModuleById(config.databaseID);
-        getParentHub().getSystemDriverRegistry().registerDatabase(system.getUniqueIdentifier(), (IObsSystemDatabase) module);
 
     }
 
     public void createOutputs(){
-        if(config.siteDiagramConfig != null){
-            siteInfoOutput = new SiteInfoOutput(system);
-            system.addOutput(siteInfoOutput, false);
-        }
+        siteInfoOutput = new SiteInfoOutput(system);
+        system.addOutput(siteInfoOutput, false);
 
 
         clientConfigOutput = new ClientConfigOutput(system);
@@ -76,10 +82,29 @@ public class OSCARServiceModule extends AbstractModule<OSCARServiceConfig> {
     protected void doStart() throws SensorHubException {
         super.doStart();
 
-        // TODO: Publish latest site info observation
-        if (config.siteDiagramConfig != null)
-            siteInfoOutput.setData(config.siteDiagramConfig.siteDiagramPath, config.siteDiagramConfig.siteLowerLeftBound, config.siteDiagramConfig.siteUpperRightBound);
+        getParentHub().getSystemDriverRegistry().register(system);
 
+        var module = getParentHub().getModuleRegistry().getModuleById(config.databaseID);
+        if (getParentHub().getSystemDriverRegistry().getDatabase(system.getUniqueIdentifier()) == null)
+            getParentHub().getSystemDriverRegistry().registerDatabase(system.getUniqueIdentifier(), (IObsSystemDatabase) module);
+
+
+        // TODO: Publish latest site info observation
+        if (config.siteDiagramConfig != null
+                && config.siteDiagramConfig.siteDiagramPath != null
+                && !config.siteDiagramConfig.siteDiagramPath.isEmpty()
+                && config.siteDiagramConfig.siteLowerLeftBound != null
+                && config.siteDiagramConfig.siteUpperRightBound != null) {
+            siteInfoOutput.setData(config.siteDiagramConfig.siteDiagramPath, config.siteDiagramConfig.siteLowerLeftBound, config.siteDiagramConfig.siteUpperRightBound);
+        }
+    }
+
+    public SitemapDiagramHandler getSitemapDiagramHandler() {
+        return sitemapDiagramHandler;
+    }
+
+    public SpreadsheetHandler getSpreadsheetHandler() {
+        return spreadsheetHandler;
     }
 
     @Override
