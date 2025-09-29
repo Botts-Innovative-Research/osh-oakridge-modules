54381dc694ff29fae41c71d49d8a6e7531794b1a
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-29 10:17:06 -0500

    added tests for charts/reports/tables, removed the file upload from sitemap for now

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/ChartGenerator.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/reports/helpers/ChartGenerator.java
@@ -3,10 +3,15 @@ package com.botts.impl.service.oscar.reports.helpers;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarPainter;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.StandardBarPainter;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
 
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
@@ -16,6 +21,7 @@ public class ChartGenerator {
     public String createChart(String title, String xAxisLabel, String yAxisLabel, DefaultCategoryDataset dataset, String chartType, String outputFilePath) throws IOException {
         JFreeChart chart = null;
 
+
         switch(chartType.toLowerCase()) {
             case "bar":
                 chart = ChartFactory.createBarChart(
@@ -23,24 +29,40 @@ public class ChartGenerator {
                         dataset,
                         PlotOrientation.VERTICAL, true, true, false
                 );
+
+                chart.getCategoryPlot().getDomainAxis().setCategoryLabelPositions(CategoryLabelPositions.UP_45);
+
+                chart.getCategoryPlot().getDomainAxis().setTickLabelFont(new Font("SansSerif", Font.PLAIN, 10));
+                chart.getCategoryPlot().getRangeAxis().setTickLabelFont(new Font("SansSerif", Font.PLAIN, 10));
+                chart.getTitle().setFont(new Font("SansSerif", Font.BOLD, 16));
+                chart.getLegend().setItemFont(new Font("SansSerif", Font.PLAIN, 12));
+
+
+
+                BarRenderer renderer = (BarRenderer) chart.getCategoryPlot().getRenderer();
+                Color color = new Color(109, 162, 231);
+                renderer.setSeriesPaint(0, color);
+
                 break;
             case "line":
                 chart = ChartFactory.createLineChart(
                         title, xAxisLabel, yAxisLabel, dataset,
                         PlotOrientation.VERTICAL, true, true, false
                 );
+
+
                 break;
             default:
                 throw new IllegalArgumentException("Unknown chart type: " + chartType);
         }
 
 
+
         if(chart != null){
+
             File file = new File("files/reports/" + outputFilePath);
             file.getParentFile().mkdirs();
-            ChartUtilities.saveChartAsPNG(file, chart, 800, 600);
-
-
+            ChartUtilities.saveChartAsPNG(file, chart, 1200, 600);
 
             return file.getAbsolutePath();
         }
