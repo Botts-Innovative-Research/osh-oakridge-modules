9add96d326d85d010bc2d38ccdd21ce30d56fe88
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-29 10:17:06 -0500

    added tests for charts/reports/tables, removed the file upload from sitemap for now

--- services/sensorhub-service-oscar/src/test/java/com/botts/impl/service/oscar/SiteDiagramTests.java
+++ services/sensorhub-service-oscar/src/test/java/com/botts/impl/service/oscar/SiteDiagramTests.java
@@ -1,4 +1,20 @@
 package com.botts.impl.service.oscar;
 
+import org.junit.Test;
+
 public class SiteDiagramTests {
+
+    @Test
+    public void fetchSiteDiagramInfo(){
+
+    }
+
+    @Test
+    public void TestCalculatingLatitude(){
+
+    }
+    @Test
+    public void TestCalculatingLongitude(){
+    }
+
 }
