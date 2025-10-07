4b8a6e697d50f466f8fc1c68809ad210df4717d9
kyle-fitzp <kyle.fitzpatrick@botts-inc.com>
2025-10-07 14:19:13 -0500

    Fix file test

--- sensors/sensorhub-driver-ffmpeg/src/test/java/org/sensorhub/impl/sensor/ffmpeg/tests/FileTestMjpeg.java
+++ sensors/sensorhub-driver-ffmpeg/src/test/java/org/sensorhub/impl/sensor/ffmpeg/tests/FileTestMjpeg.java
@@ -4,9 +4,15 @@ import org.junit.Ignore;
 import org.sensorhub.impl.sensor.ffmpeg.ConnectionTest;
 import org.sensorhub.impl.sensor.ffmpeg.FFMPEGSensor;
 import org.sensorhub.impl.sensor.ffmpeg.config.FFMPEGConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.file.Paths;
 
 
 public class FileTestMjpeg extends ConnectionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileTestMjpeg.class);
 
     public FileTestMjpeg() throws Exception {
     }
@@ -14,14 +20,20 @@ public class FileTestMjpeg extends ConnectionTest {
     @Override
     protected void populateConfig(FFMPEGConfig config) {
         config.connection.useTCP = true;
-        config.connection.fps = 24;
+        config.connection.fps = 30;
         config.name = "H264 File";
         config.serialNumber = "h264_file";
         config.autoStart = false;
-        config.connection.connectionString = FFMPEGSensor.class.getResource("sample-stream.ts").getPath().substring(1);
-        //config.connection.connectionString = "http://192.171.163.3/axis-cgi/media.cgi?audiocodec=aac&audiosamplerate=16000&audiobitrate=32000&camera=1&videoframeskipmode=empty&videozprofile=classic&resolution=1920x1080&audiodeviceid=0&audioinputid=0&timestamp=1&videocodec=h264&container=mp4";
+        try {
+            config.connection.transportStreamPath = Paths.get(FFMPEGSensor.class.getResource("sample-stream.ts").toURI()).toString();
+        } catch (Exception e) {
+            logger.error("Could not parse file path for test!");
+        }
         config.moduleClass = FFMPEGSensor.class.getCanonicalName();
         config.connectionConfig.connectTimeout = 5000;
         config.connectionConfig.reconnectAttempts = 10;
+        config.fileConfig.videoClipDirectory = "";
+        config.fileConfig.hlsDirectory = "";
+        config.connection.useHLS = false;
     }
 }
