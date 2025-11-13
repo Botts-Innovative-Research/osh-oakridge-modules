7a8b22f9b16fe64266aa0a2a27699c575ad118fc
kyle-fitzp <kyle.fitzpatrick@botts-inc.com>
2025-11-13 16:20:05 -0600

    Fix missing input timebase

--- sensors/sensorhub-driver-ffmpeg/src/main/java/org/sensorhub/impl/sensor/ffmpeg/outputs/FileOutput.java
+++ sensors/sensorhub-driver-ffmpeg/src/main/java/org/sensorhub/impl/sensor/ffmpeg/outputs/FileOutput.java
@@ -126,93 +126,6 @@ public class FileOutput<FFMPEGConfigType extends FFMPEGConfig> extends AbstractS
         }
     }
 
-    // TODO Reduce reused code
-    /**
-     * Write video data to an OutputStream. A file name is still required for determining file format and
-     * broadcasting the file name with {@link #publish()}.
-     * @param outputStream
-     * @param fileName
-     * @throws IOException
-     */
-    public void openFile(OutputStream outputStream, String fileName) throws IOException {
-        try {
-            if (doFileWrite.get()) {
-                throw new IOException("Already writing to file " + this.fileName);
-            }
-
-            this.outputStream = outputStream;
-            this.fileName = fileName;
-
-            seekableBuffer = new ByteArraySeekableBuffer(8 * 1024 * 1024); // 8 MB initial size
-            writeCallback = new WriteCallback(seekableBuffer).retainReference();
-            seekCallback = new SeekCallback(seekableBuffer).retainReference();
-
-            /*
-            filePts = 0;
-
-            //AVFormatContext inputContext = this.parentSensor.getProcessor().getAvFormatContext();
-            AVStream inputStream = this.parentSensor.getProcessor().getAvStream();
-
-            outputContext = new AVFormatContext(null);
-            avformat_alloc_output_context2(outputContext, null, null, this.fileName); // Assuming always mp4 output
-
-            outputVideoStream = avformat.avformat_new_stream(outputContext, null);
-
-            avcodec.avcodec_parameters_copy(outputVideoStream.codecpar(), inputStream.codecpar());
-
-            // We're transcoding, need to override some of the copied values
-            outputVideoStream.codecpar().codec_id(AV_CODEC_ID_H264);
-            outputVideoStream.codecpar().codec_tag(0);
-
-            outputVideoStream.time_base(timeBase);
-            outputVideoStream.duration(AV_NOPTS_VALUE);
-
-            outputVideoStream.start_time(0);
-            outputVideoStream.position(0);
-
-             */
-            AVCodecParameters avCodecParameters = this.parentSensor.getProcessor().getCodecParams();
-            if (avCodecParameters == null) {
-                logger.error("Could not open file output; this stream's codec parameters are null");
-                return;
-            }
-            initCtx(avCodecParameters);
-
-            //timeBase = (double) inputStream.time_base().num() / inputStream.time_base().den();
-
-            this.buffer = new BytePointer(avutil.av_malloc(4096)).capacity(4096);
-            var avio = avio_alloc_context(buffer, 4096, 1, null, null, writeCallback, seekCallback);
-            outputContext.pb(avio);
-            outputContext.url(new BytePointer(av_malloc(1)).putString(""));
-
-            AVDictionary options = null;
-            if (this.fileName.endsWith(".m3u8")) {
-                options = hlsOptions();
-            }
-
-            int ret;
-            if ((ret = avformat.avformat_write_header(outputContext, options)) < 0) {
-                logFFmpeg(ret);
-                throw new IOException("Could not write header to file.");
-            }
-
-            /*
-            while (!framesSinceKey.isEmpty()) {
-                AVPacket packet = framesSinceKey.pop();
-                packetTiming(packet);
-                av_write_frame(outputContext, packet);
-                av_packet_free(packet);
-            }
-
-             */
-
-            timeBase = outputVideoStream.time_base();
-            doFileWrite.set(true);
-        } catch (Exception e) {
-            throw new IOException("Could not open output file " + fileName, e);
-        }
-    }
-
     /**
      * Write video data to a file. Format determined by file name suffix.
      * @param fileName
@@ -220,70 +133,52 @@ public class FileOutput<FFMPEGConfigType extends FFMPEGConfig> extends AbstractS
      */
     public void openFile(String fileName) throws IOException {
         try {
-            if (doFileWrite.get()) {
-                throw new IOException("Already writing to file " + this.fileName);
-            }
-            //avutil.av_log_set_level(AV_LOG_DEBUG);
-
-            // TODO REMOVE REPEAT CODE
-            this.outputStream = null;
-            this.fileName = fileName;
-
-            seekableBuffer = null;
-            writeCallback = null;
-            seekCallback = null;
-
-            /*
-            filePts = 0;
-            int ret;
-
-            //AVFormatContext inputContext = this.parentSensor.getProcessor().getAvFormatContext();
-            AVStream inputStream = this.parentSensor.getProcessor().getAvStream();
-
-            outputContext = new AVFormatContext(null);
-            avformat_alloc_output_context2(outputContext, null, null, this.fileName);
-
-            outputVideoStream = avformat.avformat_new_stream(outputContext, null);
-
-            avcodec.avcodec_parameters_copy(outputVideoStream.codecpar(), inputStream.codecpar());
-
-            // We're transcoding, need to override some of the copied values
-            outputVideoStream.codecpar().codec_id(AV_CODEC_ID_H264);
-            outputVideoStream.codecpar().codec_tag(0);
-
-            outputVideoStream.time_base(timeBase);
-
-            outputVideoStream.start_time(0);
-            outputVideoStream.position(0);
-
-             */
-            AVCodecParameters avCodecParameters = this.parentSensor.getProcessor().getCodecParams();
-            if (avCodecParameters == null) {
-                logger.error("Could not open file output; this stream's codec parameters are null");
-                return;
-            }
-            initCtx(avCodecParameters);
-
-            //timeBase = (double) inputStream.time_base().num() / inputStream.time_base().den();
-
-            if ((outputContext.oformat().flags() & AVFMT_NOFILE) == 0) { // IMPORTANT! HLS does not have a pb!
-                AVIOContext avioContext = new AVIOContext(null);
-                if (avio_open(avioContext, this.fileName, AVIO_FLAG_WRITE) < 0) {
-                    throw new IOException("Could not open file.");
+            synchronized (contextLock) {
+                if (doFileWrite.get()) {
+                    throw new IOException("Already writing to file " + this.fileName);
                 }
-                outputContext.pb(avioContext);
-            }
+                //avutil.av_log_set_level(AV_LOG_DEBUG);
 
-            AVDictionary options = null;
-            if (this.fileName.endsWith(".m3u8")) {
-                options = hlsOptions();
-            }
+                // TODO REMOVE REPEAT CODE
+                this.outputStream = null;
+                this.fileName = fileName;
 
-            int ret;
-            if ((ret = avformat.avformat_write_header(outputContext, options)) < 0) {
-                logFFmpeg(ret);
-                throw new IOException("Could not write header to file.");
-            }
+                seekableBuffer = null;
+                writeCallback = null;
+                seekCallback = null;
+
+                AVCodecParameters avCodecParameters = this.parentSensor.getProcessor().getCodecParams();
+                if (avCodecParameters == null) {
+                    logger.error("Could not open file output; this stream's codec parameters are null");
+                    return;
+                }
+
+                var inStream = parentSensor.getProcessor().getAvStream();
+                if (inStream != null)
+                    inputTimeBase = parentSensor.getProcessor().getAvStream().time_base();
+                else
+                    inputTimeBase = av_make_q(1, 90000);
+
+                initCtx(avCodecParameters);
+
+                if ((outputContext.oformat().flags() & AVFMT_NOFILE) == 0) { // IMPORTANT! HLS does not have a pb!
+                    AVIOContext avioContext = new AVIOContext(null);
+                    if (avio_open(avioContext, this.fileName, AVIO_FLAG_WRITE) < 0) {
+                        throw new IOException("Could not open file.");
+                    }
+                    outputContext.pb(avioContext);
+                }
+
+                AVDictionary options = null;
+                if (this.fileName.endsWith(".m3u8")) {
+                    options = hlsOptions();
+                }
+
+                int ret;
+                if ((ret = avformat.avformat_write_header(outputContext, options)) < 0) {
+                    logFFmpeg(ret);
+                    throw new IOException("Could not write header to file.");
+                }
 
             /*
             while (!framesSinceKey.isEmpty()) {
@@ -294,9 +189,9 @@ public class FileOutput<FFMPEGConfigType extends FFMPEGConfig> extends AbstractS
             }
 
              */
-
-            timeBase = outputVideoStream.time_base();
-            doFileWrite.set(true);
+                timeBase = outputVideoStream.time_base();
+                doFileWrite.set(true);
+            }
         } catch (Exception e) {
             throw new IOException("Could not open output file " + fileName, e);
         }
@@ -312,15 +207,15 @@ public class FileOutput<FFMPEGConfigType extends FFMPEGConfig> extends AbstractS
         outputVideoStream = avformat.avformat_new_stream(outputContext, null);
 
         avcodec.avcodec_parameters_copy(outputVideoStream.codecpar(), codecParams);
-        FFmpegUtils.cleanCodecParameters(outputVideoStream.codecpar());
+        //FFmpegUtils.cleanCodecParameters(outputVideoStream.codecpar());
 
-        //inputTimeBase = avCodecParameters.time_base();
+        timeBase = av_make_q(1, 90000);
 
         // We're transcoding, need to override some of the copied values
         outputVideoStream.codecpar().codec_id(AV_CODEC_ID_H264);
         outputVideoStream.codecpar().codec_tag(0);
 
-        //outputVideoStream.time_base(timeBase);
+        outputVideoStream.time_base(timeBase);
         outputVideoStream.duration(AV_NOPTS_VALUE);
 
         outputVideoStream.start_time(0);
