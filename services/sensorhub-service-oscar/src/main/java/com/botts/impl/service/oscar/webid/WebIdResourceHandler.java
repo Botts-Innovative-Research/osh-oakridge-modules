19f379523355df815d55bc62aa5b9f2d7135be98
Alex Almanza <alexalmanza@AlexM4.local>
2026-02-03 18:47:27 -0600

    Add Web ID handler TODOs

--- services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/webid/WebIdResourceHandler.java
+++ services/sensorhub-service-oscar/src/main/java/com/botts/impl/service/oscar/webid/WebIdResourceHandler.java
@@ -2,9 +2,11 @@ package com.botts.impl.service.oscar.webid;
 
 import com.botts.api.service.bucket.IBucketStore;
 import com.botts.impl.service.bucket.handler.DefaultObjectHandler;
+import com.botts.impl.service.bucket.util.RequestContext;
 import com.botts.impl.service.oscar.cambio.CambioConverter;
 import com.google.common.collect.ImmutableCollection;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
@@ -26,7 +28,23 @@ public class WebIdResourceHandler extends DefaultObjectHandler {
         pattern = Pattern.compile(regex);
     }
 
+    @Override
+    public void doPut(RequestContext ctx) throws IOException, SecurityException {
+        super.doPut(ctx);
+    }
 
+    @Override
+    public void doPost(RequestContext ctx) throws IOException, SecurityException {
+        super.doPost(ctx);
+        // TODO: Take in query parameters to attach WebID analysis to occupancy, and output WebID analysis to proper lane
+        /**
+         * occupancyObsId
+         * laneUid
+         * webIdEnabled
+         */
+
+        // TODO: super method, if web enabled then send web ID request, and attach web ID results to occupancy
+    }
 
     @Override
     public String getObjectPattern() {
